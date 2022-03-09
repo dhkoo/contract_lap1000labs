b3b55ef644b0499ca74fde16711780af3d7ff3d4
dhkoo <amainlog@gmail.com>
2022-03-09 16:09:46 +0900

    chore: upradeable alapRegistration migration

--- migrations/10_alapRegistration_migration.js
+++ migrations/10_alapRegistration_migration.js
@@ -1,5 +1,17 @@
+const ConfigYaml = require("config-yaml");
+const contracts = ConfigYaml("../lap_config/contracts.yaml");
+const Proxy = artifacts.require("Proxy");
 const AlapRegistration = artifacts.require("AlapRegistration");
 
 module.exports = async function (deployer, network, accounts) {
+  await deployer.deploy(Proxy);
   await deployer.deploy(AlapRegistration);
+
+  const proxy = await Proxy.deployed();
+  const logic = await AlapRegistration.deployed();
+
+  await proxy.upgrade(logic.address);
+
+  const alapRegistration = await AlapRegistration.at(proxy.address);
+  await alapRegistration.initialize(contracts.cypress.alap);
 };
